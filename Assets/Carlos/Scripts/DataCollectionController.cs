2a989d44e633417e87a60c979ef3bb02f256f38d
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-03-03 12:03:59 +0000

    Added DataCollectionController script to scene and connected it to the IMLGraph

---
+++ Assets/Carlos/Scripts/DataCollectionController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+
+namespace MECM
+{
+    /// <summary>
+    /// Controls when data starts being collected and when it stops being collected
+    /// </summary>
+    public class DataCollectionController : MonoBehaviour
+    {
+
+        #region Variables
+
+        private bool m_StartDataCollection;
+
+        private bool m_StopDataCollection;
+
+        /// <summary>
+        /// Event that toggles data collection on/off
+        /// </summary>
+        [SendToIMLGraph]
+        public bool ToggleDataCollection;
+
+        #endregion
+
+        #region Unity Messages
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #endregion
+
+    }
+
+}
