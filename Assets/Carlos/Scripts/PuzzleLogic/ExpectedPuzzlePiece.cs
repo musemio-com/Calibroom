819c56726bd8f9fb3e9990c0a8a1cd37b4e4b0b3
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-03-16 19:44:08 +0000

    First iteration of puzzle logic
    - Introduced piece detector sensor
    - PuzzlePiece component
    - PuzzleBoard component
    - Sketched first tutorial room

---
+++ Assets/Carlos/Scripts/PuzzleLogic/ExpectedPuzzlePiece.cs
@@ -0,0 +1,17 @@
+﻿/// <summary>
+/// Container of info for expected puzzle piece
+/// </summary>
+[System.Serializable]
+public class ExpectedPuzzlePiece
+{
+    /// <summary>
+    /// The ID of this particular piece
+    /// </summary>
+    public string ID;
+
+    /// <summary>
+    /// Is the piece in correct place?
+    /// </summary>
+    public bool InPlace;
+
+}
---
+++ Assets/Carlos/Scripts/PuzzleLogic/PuzzlePiece.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages the data of a PuzzlePiece
+/// </summary>
+public class PuzzlePiece : MonoBehaviour
+{
+    /// <summary>
+    /// The ID of this particular piece
+    /// </summary>
+    public string ID;
+
+
+    /// <summary>
+    /// Is the piece in correct place?
+    /// </summary>
+    public bool InPlace;
+
+    /// <summary>
+    /// Is the piece on the table?
+    /// </summary>
+    public bool OnTable;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PuzzleBoard table = collision.gameObject.GetComponent<PuzzleBoard>();
+        // If we have collided with the table...
+        if (table != null)
+        {
+            // Set flag to true
+            OnTable = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        PuzzleBoard table = collision.gameObject.GetComponent<PuzzleBoard>();
+        // If we left the table...
+        if (table != null)
+        {
+            // Set flag to false
+            OnTable = false;
+        }
+
+    }
+
+}
