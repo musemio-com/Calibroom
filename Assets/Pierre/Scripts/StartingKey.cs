aeee4ca77d99796a3fe5f43c968dc39fea01391c
PierreRider <31930617+PierreRider@users.noreply.github.com>
2021-03-23 21:42:07 +0000

    StartKey script + deadlimit + RoomB

--- Assets/Pierre/Scripts/StartingKey.cs
+++ Assets/Pierre/Scripts/StartingKey.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// manage the behavior of the starting key
+
 public class StartingKey : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool pieceColor = false;
+
+    /// <summary>
+    /// Saved info from original transform
+    /// </summary>
+    private Vector3 m_OriginalPosition;
+    private Quaternion m_OriginalRotation;
+
+    private void Start()
     {
-        
+        // Save original transform if piece needs to reset (i.e. out of room limits)
+        m_OriginalPosition = transform.position;
+        m_OriginalRotation = transform.rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
-    }
+        //PuzzleBoard table = collision.gameObject.GetComponent<PuzzleBoard>();
+        //// If we have collided with the table...
+        //if (table != null)
+        //{
+        //    // Set flag to true
+        //    OnTable = true;
+        //}
+
+        DeadLimit deadLimit = collision.gameObject.GetComponent<DeadLimit>();
+        // If we collide with a dead limit...
+        if (deadLimit != null)
+        {
+            // Reset piece to original transform
+            transform.position = m_OriginalPosition;
+            transform.rotation = m_OriginalRotation;
+        }
+    }//
 }
