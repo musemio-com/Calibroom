7e889b6403f8c7c4ff0a778dad02f2b847683535
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-03-23 20:13:05 +0000

    Mock Up Select ID screen UI inside of sceneOpen_Carlos

---
+++ Assets/Carlos/Scripts/UI/UIConfirmIDController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Manages logic of Confrim ID UI Screen
+/// </summary>
+public class UIConfirmIDController : MonoBehaviour
+{
+    public TMP_Text UserID;
+
+    /// <summary>
+    /// Activates/Deactivates the screen
+    /// </summary>
+    /// <param name="option"></param>
+    public void SetActive(bool option, string userID = "0")
+    {
+        if (UserID != null)
+        {
+            this.gameObject.SetActive(option);
+            UserID.text = userID;
+        }
+    }
+}
---
+++ Assets/Carlos/Scripts/UI/UISelectIDController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Manages logic of Select ID UI Screen
+/// </summary>
+public class UISelectIDController : MonoBehaviour
+{
+    public UIDigitController DigitLeft;
+    public UIDigitController DigitRight;
+
+    /// <summary>
+    /// Activates/Deactivates the screen
+    /// </summary>
+    /// <param name="option"></param>
+    public void SetActive(bool option)
+    {
+        this.gameObject.SetActive(option);
+    }
+
+    public string GetUserID()
+    {
+        if(DigitLeft!=null && DigitRight!=null)
+        {
+            string userID = DigitLeft.GetDigit() + DigitRight.GetDigit();
+            return userID;
+        }
+        return "0";
+    }
+
+}
