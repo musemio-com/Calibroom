edf694bb0e7ae6f8e73407fb16f3b66c472485ed
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-03-25 17:06:21 +0000

    Implemented exit game through new input system package

---
+++ Assets/Carlos/Scripts/InputActionsController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Manages the behaviour of Input Actions
+/// </summary>
+public class InputActionsController : MonoBehaviour
+{
+    /// <summary>
+    /// Collection of action events available in the input system (generated through the editor)
+    /// </summary>
+    public CalibroomInputActions CalibroomActions;
+
+    private void Awake()
+    {
+        // Create instance on awake
+        if (CalibroomActions == null)
+            CalibroomActions = new CalibroomInputActions();
+
+        // Assign a delegate with the ExitGame() method to the ExitGame action
+        CalibroomActions.Menu.ExitGame.performed += x => QuitGame();
+    }
+
+    private void OnEnable()
+    {
+        // We need to manually enable input actions
+        CalibroomActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // We need to manually disable input actions
+        CalibroomActions.Disable();
+    }
+
+    /// <summary>
+    /// Quits the game completely
+    /// </summary>
+    private void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        // Account for editor applications by forcing to leave playmode
+        UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+    }
+}
