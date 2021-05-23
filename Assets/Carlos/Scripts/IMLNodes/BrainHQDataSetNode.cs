df2683d461181bcafe9691532bfd0dd22d24afdc
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-05-23 21:55:00 +0100

    BrainHQ data now loads correctly and asynchonously with a custom IML node

---
+++ Assets/Carlos/Scripts/IMLNodes/BrainHQDataSetNode.cs
@@ -0,0 +1,316 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using InteractML;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using System.Linq;
+
+namespace MECM
+{
+    /// <summary>
+    /// Holds multiple BrainHQ files
+    /// </summary>
+    [NodeWidth(300)]
+
+    public class BrainHQDataSetNode : IMLNode
+    {
+
+        #region Variables
+
+        /// <summary>
+        /// Data set containing BrainHQ info per user
+        /// </summary>
+        [Output]
+        public List<BrainHQUserData> BrainHQDataSet;
+
+        /// <summary>
+        /// Path of folder to search 
+        /// </summary>
+        public string FolderPath;
+
+        // Flags for loading
+        public bool LoadingStarted { get { return m_LoadingStarted; } }
+        [System.NonSerialized]
+        private bool m_LoadingStarted;
+        public bool LoadingFinished { get { return m_LoadingFinished; } }
+        [System.NonSerialized]
+        private bool m_LoadingFinished;
+
+
+        #endregion
+
+        // Use this for initialization
+        protected override void Init()
+        {
+            base.Init();
+
+        }
+
+        // Return the correct value of an output port when requested
+        public override object GetValue(NodePort port)
+        {
+            return null; // Replace this
+        }
+
+        #region Public Methods
+
+        /// <summary>
+        /// Loads several BrainHQ files from a folder
+        /// </summary>
+        /// <param name="path">path of file to load</param>
+        /// <param name="specificID">When loading, are we looking only for a specific nodeID in file(s)?</param>        
+        public void LoadDataSets(string path)
+        {
+            if (m_LoadingStarted)
+            {
+                NodeDebug.LogWarning("Can't start loading when there is a loading in progress...", this);
+                return;
+            }
+            if (Directory.Exists(path))
+            {
+                m_LoadingStarted = true;
+                m_LoadingFinished = false;
+
+                // init data set list
+                if (BrainHQDataSet == null)
+                    BrainHQDataSet = new List<BrainHQUserData>();
+                else
+                    BrainHQDataSet.Clear();
+
+
+                Task.Run(async () =>
+                {
+                    // First, find all the folders
+                    // Iterate to upload all files in folder, including subdirectories
+                    string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+                    Debug.Log($"{files.Length + 1} files found. Loading BrainHQ data sets, please wait...");
+
+                    Parallel.ForEach(files, async file => 
+                    {
+                        // If there is a json file, attempt to load
+                        if (Path.GetExtension(file) == ".csv")
+                        {
+                            // The entire CSV in one line
+                            string line = "";
+                            // File ID
+                            string fileName = Path.GetFileNameWithoutExtension(file);
+                            string userID = new String(fileName.Where(Char.IsDigit).ToArray());
+
+                            // Async read and extracting relevant brainhq entries from file
+                            using (var reader = File.OpenText(file))
+                            {
+                                Debug.Log($"Opening file {fileName}");
+                                line = await reader.ReadToEndAsync();
+                                // Do something with fileText...
+                                if (!string.IsNullOrEmpty(line))
+                                {
+                                    var rows = line.Split('\n');
+
+                                    // BrainHQ vars
+                                    string doubleDecision = "";
+                                    string mindBender = "";
+                                    string rightTurn = "";
+                                    string mentalMap = "";
+                                    string targetTracker = "";
+
+                                    // Extract the desired values from each column
+                                    foreach (var row in rows)
+                                    {
+                                        if (!string.IsNullOrEmpty(row))
+                                        {
+                                            // Speed/Processing Task //
+                                            // Double Decision
+                                            if (row.Contains("Double Decision"))
+                                                doubleDecision = GetValueFromRow(row, 5);
+
+                                            // Mind Bender
+                                            if (row.Contains("Mind Bender"))
+                                                mindBender = GetValueFromRow(row, 5);
+
+                                            // Visuo-Spatial Processing //
+                                            // Right Turn
+                                            if (row.Contains("Right Turn"))
+                                                rightTurn = GetValueFromRow(row, 5);
+
+                                            // Mental Map
+                                            if (row.Contains("Mental Map"))
+                                                mentalMap = GetValueFromRow(row, 5);
+
+                                            // Target Tracker
+                                            if (row.Contains("Target Tracker"))
+                                                targetTracker = GetValueFromRow(row, 5);
+
+                                        }
+                                    }
+
+                                    // Store brainHQ user data
+                                    BrainHQUserData data = new BrainHQUserData(int.Parse(userID),
+                                        float.Parse(doubleDecision), float.Parse(mindBender),
+                                        float.Parse(rightTurn), int.Parse(mentalMap), float.Parse(targetTracker));
+
+                                    // Add to list if not null
+                                    if (BrainHQDataSet != null)
+                                    {
+                                        BrainHQDataSet.Add(data);
+                                        Debug.Log($"Added brainHQ data to list {fileName}");
+
+                                    }
+
+                                }
+                                // close file
+                                reader.Close();
+                                Debug.Log($"Closing file {fileName}");
+
+                            }
+
+                        }
+
+                    });
+
+                    #region ForEach normal
+                    //foreach (string file in files)
+                    //{
+                    //    // If there is a json file, attempt to load
+                    //    if (Path.GetExtension(file) == ".csv")
+                    //    {
+                    //        // Are we looking for a specific ID?
+                    //        if (string.IsNullOrEmpty(specificID))
+                    //        {
+                    //            // skip if the file doesn't contain the ID we want
+                    //            if (!file.Contains(specificID))
+                    //                continue;
+                    //        }
+
+                    //        // The entire CSV in one line
+                    //        string line = "";
+                    //        // File ID
+                    //        string fileName = Path.GetFileNameWithoutExtension(file);
+                    //        string userID = new String(fileName.Where(Char.IsDigit).ToArray());
+
+                    //        // Async read and extracting relevant brainhq entries from file
+                    //        using (var reader = File.OpenText(file))
+                    //        {
+                    //            Debug.Log($"Opening file {fileName}");
+                    //            line = await reader.ReadToEndAsync();
+                    //            // Do something with fileText...
+                    //            if (!string.IsNullOrEmpty(line))
+                    //            {
+                    //                var rows = line.Split('\n');
+
+                    //                // BrainHQ vars
+                    //                string doubleDecision = "";
+                    //                string mindBender = "";
+                    //                string rightTurn = "";
+                    //                string mentalMap = "";
+                    //                string targetTracker = "";
+
+                    //                // Extract the desired values from each column
+                    //                foreach (var row in rows)
+                    //                {
+                    //                    if (!string.IsNullOrEmpty(row))
+                    //                    {
+                    //                        // Speed/Processing Task //
+                    //                        // Double Decision
+                    //                        if (row.Contains("Double Decision"))
+                    //                            doubleDecision = GetValueFromRow(row, 5);
+
+                    //                        // Mind Bender
+                    //                        if (row.Contains("Mind Bender"))
+                    //                            mindBender = GetValueFromRow(row, 5);
+
+                    //                        // Visuo-Spatial Processing //
+                    //                        // Right Turn
+                    //                        if (row.Contains("Right Turn"))
+                    //                            rightTurn = GetValueFromRow(row, 5);
+
+                    //                        // Mental Map
+                    //                        if (row.Contains("Mental Map"))
+                    //                            mentalMap = GetValueFromRow(row, 5);
+
+                    //                        // Target Tracker
+                    //                        if (row.Contains("Target Tracker"))
+                    //                            targetTracker = GetValueFromRow(row, 5);
+
+                    //                    }
+                    //                }
+
+                    //                // Store brainHQ user data
+                    //                BrainHQUserData data = new BrainHQUserData(int.Parse(userID),
+                    //                    float.Parse(doubleDecision), float.Parse(mindBender),
+                    //                    float.Parse(rightTurn), int.Parse(mentalMap), float.Parse(targetTracker));
+
+                    //                // Add to list if not null
+                    //                if (BrainHQDataSet != null)
+                    //                {
+                    //                    BrainHQDataSet.Add(data);
+                    //                    m_DataSetSize++;
+                    //                    Debug.Log($"Added brainHQ data to list {fileName}");
+
+                    //                }
+
+                    //            }
+                    //            // close file
+                    //            reader.Close();
+                    //            Debug.Log($"Closing file {fileName}");
+
+                    //        }
+
+                    //    }
+
+                    //}
+                    #endregion
+
+
+                    if (BrainHQDataSet.Count == 0)
+                    {
+                        NodeDebug.LogWarning("Couldn't load folder!", this);
+
+                    }
+                    else
+                    {
+                        m_LoadingFinished = true;
+                        m_LoadingStarted = false; // allow to re-load if user wants to
+                        Debug.Log($"{BrainHQDataSet.Count + 1} BrainHQ Files Loaded!");
+                    }
+
+                });
+
+            }
+            else
+            {
+                NodeDebug.LogWarning("The folder doesn't exist!", this);
+            }
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Extracts a value from a CSV row
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private string GetValueFromRow(string row, int index) 
+        {            
+            // Get threshold value
+            string[] entries = row.Split(',');
+            string value = null;
+            if (entries.Length > index && !string.IsNullOrEmpty(entries[index]))
+            {
+                value = entries[index];
+            }
+
+            return value;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Assets/Carlos/Scripts/IMLNodes/Editor/BrainHQDataSetNodeEditor.cs
@@ -0,0 +1,213 @@
+﻿using UnityEngine;
+using InteractML;
+using MECM;
+#if UNITY_EDITOR
+using UnityEditor;
+using XNodeEditor;
+#endif
+
+namespace MECM
+{
+    [CustomNodeEditor(typeof(BrainHQDataSetNode))]
+    public class BrainHQDataSetNodeEditor : IMLNodeEditor
+    {
+        /// <summary>
+        /// Node to draw
+        /// </summary>
+        BrainHQDataSetNode m_NodeDataSet;
+
+        /// <summary>
+        /// Boolean that shows or hides training data sets
+        /// </summary>
+        protected bool m_ShowDataSetDropdown;
+        /// <summary>
+        /// List of dropdowns per data entry
+        /// </summary>
+        protected bool[] m_DataDropdowns;
+        /// <summary>
+        /// Rect for dropdown layout
+        /// </summary>
+        protected Rect m_Dropdown;
+        /// <summary>
+        /// Position of scroll for dropdown
+        /// </summary>
+        protected Vector2 m_ScrollPos;
+
+        /// <summary>
+        /// Style of foldout
+        /// </summary>
+        GUIStyle m_FoldoutStyle;
+        GUIStyle m_FoldoutEmptyStyle;
+        GUIStyle m_ScrollViewStyle;
+
+        public override void OnHeaderGUI()
+        {
+            base.OnHeaderGUI();
+
+            // get refs
+            if (m_NodeDataSet == null)
+                m_NodeDataSet = target as BrainHQDataSetNode;
+            if (m_FoldoutStyle == null)
+                SetDropdownStyle(out m_FoldoutStyle);
+            if (m_FoldoutEmptyStyle == null)
+                m_FoldoutEmptyStyle = Resources.Load<GUISkin>("GUIStyles/InteractMLGUISkin").GetStyle("foldoutempty");
+            if (m_ScrollViewStyle == null)
+                m_ScrollViewStyle = Resources.Load<GUISkin>("GUIStyles/InteractMLGUISkin").GetStyle("scrollview");
+        }
+
+        public override void OnBodyGUI()
+        {
+            base.OnBodyGUI();
+
+            // We modify indent level since it seems to be slightly off for this node?
+            EditorGUI.indentLevel++;
+
+            // Button LoadDataSets
+            if (GUILayout.Button("Load BrainHQ Files", m_NodeSkin.GetStyle("Run")))
+            {
+                m_NodeDataSet.LoadDataSets(m_NodeDataSet.FolderPath);
+
+            }
+
+            // Show data sets dropdown
+            ShowDataSetsDropdown();
+
+            EditorGUI.indentLevel++;
+
+
+        }
+
+        /// <summary>
+        /// Set style for dropdown for training examples nodes
+        /// </summary>
+        protected void SetDropdownStyle(out GUIStyle myFoldoutStyle)
+        {
+            GUI.skin = Resources.Load<GUISkin>("GUIStyles/InteractMLGUISkin");
+            myFoldoutStyle = new GUIStyle(EditorStyles.foldout);
+            Color myStyleColor = Color.white;
+            myFoldoutStyle.fontStyle = FontStyle.Bold;
+            myFoldoutStyle.normal.textColor = myStyleColor;
+            myFoldoutStyle.onNormal.textColor = myStyleColor;
+            myFoldoutStyle.hover.textColor = myStyleColor;
+            myFoldoutStyle.onHover.textColor = myStyleColor;
+            myFoldoutStyle.focused.textColor = myStyleColor;
+            myFoldoutStyle.onFocused.textColor = myStyleColor;
+            myFoldoutStyle.active.textColor = myStyleColor;
+            myFoldoutStyle.onActive.textColor = myStyleColor;
+            myFoldoutStyle.fontStyle = FontStyle.Bold;
+            myFoldoutStyle.normal.textColor = myStyleColor;
+            myFoldoutStyle.fontStyle = Resources.Load<GUISkin>("GUIStyles/InteractMLGUISkin").GetStyle("scrollview").fontStyle;
+        }
+
+        private void ShowDataSetsDropdown()
+        {
+            int numEntries = 0;
+            if (m_NodeDataSet.BrainHQDataSet != null)
+                numEntries = m_NodeDataSet.BrainHQDataSet.Count;
+
+            m_ShowDataSetDropdown = EditorGUILayout.Foldout(m_ShowDataSetDropdown, $"View BrainHQ DataSet ({numEntries} Entries)", m_FoldoutStyle);
+
+            if (m_ShowDataSetDropdown)
+            {
+                //m_Dropdown = m_HelpRect;
+
+                //m_Dropdown.height = 300;
+                //if (Event.current.type == EventType.Layout)
+                //{
+                //    GUI.DrawTexture(m_Dropdown, NodeColor);
+                //}
+
+                //GUILayout.BeginArea(m_Dropdown);
+
+                EditorGUI.indentLevel++;
+
+                if (m_NodeDataSet.BrainHQDataSet == null)
+                    return;
+
+                if (m_NodeDataSet.BrainHQDataSet.Count < 0)
+                {
+                    EditorGUILayout.LabelField("BrainHQ Data Set is empty", m_FoldoutEmptyStyle);
+                }
+                else
+                {
+                    // Begins Vertical Scroll
+                    int indentLevel = EditorGUI.indentLevel;
+                    EditorGUILayout.BeginVertical();
+
+                    m_ScrollPos = EditorGUILayout.BeginScrollView(m_ScrollPos, GUILayout.Width(GetWidth() - 25), GUILayout.Height(GetWidth() - 100));
+
+                    // Make array of dropdowns match number of entries in dataset
+                    if (m_DataDropdowns == null || m_DataDropdowns.Length != m_NodeDataSet.BrainHQDataSet.Count)
+                        m_DataDropdowns = new bool[m_NodeDataSet.BrainHQDataSet.Count];
+
+                    // Iterate entries 
+                    for (int i = 0; i < m_NodeDataSet.BrainHQDataSet.Count; i++)
+                    {
+                        EditorGUILayout.LabelField($"BrainHQ User Data Set {i}", m_ScrollViewStyle);
+
+                        var brainHQData = m_NodeDataSet.BrainHQDataSet[i];
+                        
+                        EditorGUI.indentLevel++;
+
+                        string userID = brainHQData.ID.ToString();
+
+                        m_DataDropdowns[i] = EditorGUILayout.Foldout(m_DataDropdowns[i], $"User ID: {userID}", m_FoldoutStyle);
+
+                        // we only draw inputs if there are more than 5 examples
+                        if (m_DataDropdowns[i])
+                        {
+                            EditorGUI.indentLevel++;
+
+                            EditorGUILayout.LabelField("Speed/Processing Task", m_ScrollViewStyle);
+
+                            EditorGUI.indentLevel++;
+                                // Speed/Processing Task //
+                                // Double Decision
+                                EditorGUILayout.LabelField($"Double Decision: {brainHQData.DoubleDecisionScore} ", m_ScrollViewStyle);
+
+                                // Mind Bender
+                                EditorGUILayout.LabelField($"Mind Bender: {brainHQData.MindBenderScore} ", m_ScrollViewStyle);
+                            EditorGUI.indentLevel--;
+
+                            EditorGUILayout.LabelField("Visuo-Spatial Processing", m_ScrollViewStyle);
+
+                            EditorGUI.indentLevel++;
+                                // Visuo-Spatial Processing //
+                                // Right Turn
+                                EditorGUILayout.LabelField($"Right Turn: {brainHQData.RightTurnScore} ", m_ScrollViewStyle);
+
+                                // Mental Map
+                                EditorGUILayout.LabelField($"Mental Map: {brainHQData.MentalMapScore} ", m_ScrollViewStyle);
+
+                                // Target Tracker
+                                EditorGUILayout.LabelField($"Target Tracker: {brainHQData.TargetTracker} ", m_ScrollViewStyle);
+                            EditorGUI.indentLevel--;
+                            
+                            EditorGUI.indentLevel--;
+
+
+                        }
+
+                        EditorGUI.indentLevel--;
+
+                    }
+
+                    // Ends Vertical Scroll
+                    EditorGUI.indentLevel = indentLevel;
+                    EditorGUILayout.EndScrollView();
+                    EditorGUILayout.EndVertical();
+                }
+
+
+
+            }
+
+            EditorGUI.indentLevel--;
+            //GUILayout.EndArea();
+
+
+        }
+
+    }
+
+}
