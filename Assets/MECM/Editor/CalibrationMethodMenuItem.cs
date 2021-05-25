1db7de96473cf47a657ade649d2becd0a40a6073
Saifiction <saif_fiction@outlook.com>
2021-05-25 22:50:21 +0100

    Initial Dashboard Interface.

---
+++ Assets/MECM/Editor/CalibrationMethodMenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CalibrationMethodMenuItem
+{
+    [MenuItem("MECM/Setup Calibration Method")]
+    public static void Execute()
+    {
+        UnityEngine.GameObject obj =  UnityEngine.MonoBehaviour.Instantiate(AssetDatabase.LoadAssetAtPath("Assets/MECM/Prefabs/Data/Calibration Method.prefab", typeof(GameObject))) as GameObject;
+        obj.name = "Calibration Method";
+        Selection.activeGameObject = obj;
+
+    }
+}
---
+++ Assets/MECM/Scripts/Data/CalibrationMethodManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using MECM;
+using InteractML;
+using UnityEngine.SceneManagement;
+public class CalibrationMethodManager : MonoBehaviour
+{
+    [Header("User to collect Data From")]
+    public int UserID;
+    [Header("Right Hand - Left Hand - HMD")]
+    public TrackedObjects trackedObjects;
+    public bool UploadToFirebase;
+    [DrawIf("UploadToFirebase",true)]
+    public FirebaseCredentialsObject FirebaseCredentials;
+    public void InitializeMECM()
+    {
+        DataCollectionController DTC = FindObjectOfType<DataCollectionController>();
+        IMLComponent ImlComponent = FindObjectOfType<IMLComponent>();
+
+        FindObjectOfType<UserDetailsController>().UserData.UserID = UserID;
+        DTC.UserIDInt = UserID;
+        DTC.UserIDString = "" + FindObjectOfType<UserDetailsController>().LoadUserID() + "/" + SceneManager.GetActiveScene().name;
+        DTC.RightHandGrabbingExtractor = trackedObjects.RightHand.AddComponent<GrabbingPieceFeatureExtractor>();
+        DTC.LeftHandGrabbingExtractor = trackedObjects.LeftHand.AddComponent<GrabbingPieceFeatureExtractor>();
+        ImlComponent.GameObjectsToUse.Clear();
+        ImlComponent.GameObjectsToUse.Insert(0,trackedObjects.HMD);
+        ImlComponent.GameObjectsToUse.Insert(1,trackedObjects.RightHand);
+        ImlComponent.GameObjectsToUse.Insert(2,trackedObjects.LeftHand);
+
+        //SetupFirebaseCredentials();
+    }
+    //void SetupFirebaseCredentials()
+    //{
+    //    FirebaseController fbController = FindObjectOfType<FirebaseController>();
+    //    fbController.StorageBucket = FirebaseCredentials.StorageBucket;
+    //    fbController.CredentialsPath = FirebaseCredentials.CredentialsPath;
+    //    fbController.PathToUpload = FirebaseCredentials.PathToUpload;
+    //}
+}
+
+[Serializable]
+public class FirebaseCredentialsObject
+{
+    public string StorageBucket;
+    public string CredentialsPath;
+    public string PathToUpload;
+}
+
+[Serializable]
+public class TrackedObjects
+{
+    public GameObject RightHand;
+    public GameObject LeftHand;
+    public GameObject HMD;
+}
