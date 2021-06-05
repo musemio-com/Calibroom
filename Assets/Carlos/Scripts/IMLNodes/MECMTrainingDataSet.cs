d7663951fc1cbba168a7e987bb78317bb280f59b
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-06-05 10:56:00 +0100

    MECM Training Data Set async processing

--- Assets/Carlos/Scripts/IMLNodes/MECMTrainingDataSet.cs
+++ Assets/Carlos/Scripts/IMLNodes/MECMTrainingDataSet.cs
@@ -29,6 +29,10 @@ namespace MECM
         /// </summary>
         [Input]
         public List<BrainHQUserData> BrainHQDataSet;
+        
+        public int DataSetsProcessed;
+        private bool m_ProcessingStarted;
+        private bool m_ProcessingFinished;
 
         #endregion
 
@@ -49,6 +53,21 @@ namespace MECM
 
         #endregion
 
+        #region Public Methods
+
+        /// <summary>
+        /// Chunks a list of trainingDataSets + BrainHQData into windows of given size 
+        /// Assumes by default that one window is 10 samples (10 samples per sec, 1 window = 1 sec)
+        /// </summary>
+        /// <param name="inputData"></param>
+        /// <param name="lengthWindow"></param>
+        public void DataToWindows()
+        {
+            Task.Run(async () => await DataToWindowsAsync(MovementData, BrainHQDataSet) );
+        }
+
+        #endregion
+
         #region Private Methods
 
         /// <summary>
@@ -57,55 +76,102 @@ namespace MECM
         /// </summary>
         /// <param name="inputData"></param>
         /// <param name="lengthWindow"></param>
-        private void DataToWindows(List<List<IMLTrainingExample>> inputData, List<BrainHQUserData> outputData, int lengthWindow = 10)
+        private async Task<List<List<IMLTrainingExample>>> DataToWindowsAsync(List<List<IMLTrainingExample>> inputData, List<BrainHQUserData> outputData, int lengthWindow = 10)
         {
             if (inputData == null)
-                return;
+            {
+                NodeDebug.LogWarning("Input Data is null!", this);
+                return null;
+            }
+            if (outputData == null)
+            {
+                NodeDebug.LogWarning("Output Data is null!", this);
+                return null;
+            }
+            if (m_ProcessingStarted)
+            {
+                NodeDebug.LogWarning("Data is already processing! Cannot start twice", this);
+                return null;
+            }
 
+            DataSetsProcessed = 0;
+            m_ProcessingStarted = true;
+            m_ProcessingFinished = false;
             List<List<IMLTrainingExample>> windowedData = new List<List<IMLTrainingExample>>();
-
             // Iterate through one dataset at a time
             foreach (List<IMLTrainingExample> trainingExamples in inputData)
             {
+                if (trainingExamples == null)
+                {
+                    NodeDebug.LogWarning("There is a null DataSet in collection! Aborting processing", this);
+                    m_ProcessingStarted = false;
+                    return null;
+                }
+
                 // Select the BrainHQ dataset that matches the user ID of this dataset
                 var brainHQLabel = outputData.First(x => x.ID == trainingExamples[0].Outputs[0].OutputData.Values[0]);
                 
-
-                // TODO CREATE MORE WINDOWS!!!
-
                 // Chunk training Examples in new list
-                int index = 0; // TODO we need to have a for loop to increase this index. RIGHT NOW WE ONLY MAKE 1 WINDOW!!
                 List<IMLTrainingExample> windowedDataSet = new List<IMLTrainingExample>();
-                IMLTrainingExample newExampleWindow = new IMLTrainingExample();
                 
-                // Add examples to window
-                for (int i = 0; i < lengthWindow; i++)
+                // We chunk N examples together in one window (newExampleWindow). Move the index up the size of the window 
+                for (int i = 0; i < trainingExamples.Count; i += lengthWindow)
                 {
-                    // Add example input data to window
-                    var example = trainingExamples[i];
-                    if (example != null) 
+                    IMLTrainingExample newExampleWindow = new IMLTrainingExample();
+                    // Get our specific window boundaries (starting from last window last index)
+                    int localLengthWindow = i + lengthWindow;
+                    // Check that there are enough inputs left for window (in case this is the last window). If not, we skip this window
+                    if (localLengthWindow > trainingExamples.Count)
+                        break;
+
+                    // Add examples to window (start on last entry of dataset list)
+                    for (int j = i; j < localLengthWindow; j++)
                     {
-                        foreach (var input in example.Inputs)
+                        // Add example input data to window
+                        var example = trainingExamples[j];
+                        if (example != null)
                         {
-                            newExampleWindow.AddInputExample(input.InputData);
+                            foreach (var input in example.Inputs)
+                            {
+                                newExampleWindow.AddInputExample(input.InputData);
+                            }
+                        }
+                        else
+                        {
+                            NodeDebug.LogWarning("Null training example in DataSet! Aborting processing", this);
+                            m_ProcessingStarted = false;
+                            return null;
+
+                        }
+
+                        // If it is the last input added to window...
+                        if (j == localLengthWindow - 1)
+                        {
+                            // Add the BrainHQ data as the output for the entire window
+                            // User ID
+                            newExampleWindow.AddOutputExample(new IMLInteger(brainHQLabel.ID));
+                            // Speed/Processing Task //   
+                            newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.DoubleDecisionScore));
+                            newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.MindBenderScore));
+                            // Visuo-Spatial Processing //
+                            newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.RightTurnScore));
+                            newExampleWindow.AddOutputExample(new IMLInteger(brainHQLabel.MentalMapScore));
+                            newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.TargetTracker));
                         }
                     }
-                    // If it is the last input added to window...
-                    if (i == lengthWindow - 1)
-                    {
-                        // Add the BrainHQ data as the output for the entire window
-                        // User ID
-                        newExampleWindow.AddOutputExample(new IMLInteger(brainHQLabel.ID));
-                        // Speed/Processing Task //   
-                        newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.DoubleDecisionScore));
-                        newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.MindBenderScore));
-                        // Visuo-Spatial Processing //
-                        newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.RightTurnScore));
-                        newExampleWindow.AddOutputExample(new IMLInteger(brainHQLabel.MentalMapScore));
-                        newExampleWindow.AddOutputExample(new IMLFloat(brainHQLabel.TargetTracker));
-                    }
+
+                    // Add window to training data set
+                    windowedDataSet.Add(newExampleWindow);
+                    DataSetsProcessed++;
                 }
+
+                // Add windowed dataSet to collection of dataSets
+                windowedData.Add(windowedDataSet);
             }
+
+            m_ProcessingFinished = true;
+            m_ProcessingStarted = false;
+            return windowedData;
         }
 
         #endregion
