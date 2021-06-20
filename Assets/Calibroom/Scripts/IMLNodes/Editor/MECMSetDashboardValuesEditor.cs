3a7b3994861fd830eb7a1eec8b4c869a9406f74c
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-06-20 21:53:03 +0100

    Model now sending info to dashboard

---
+++ Assets/Calibroom/Scripts/IMLNodes/Editor/MECMSetDashboardValuesEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using InteractML;
+using MECM;
+#if UNITY_EDITOR
+using UnityEditor;
+using XNodeEditor;
+#endif
+
+namespace MECM
+{
+    [CustomNodeEditor(typeof(MECMSetDashboardValues))]
+    public class MECMSetDashboardValuesEditor : IMLNodeEditor
+    {
+        public override void OnBodyGUI()
+        {
+            base.OnBodyGUI();
+
+            var setValuesNode = target as MECMSetDashboardValues;
+            // Set values on dashboard window
+            if (setValuesNode != null)
+                DashboardEditorWindow.SetupScore
+                    (
+                    setValuesNode.OverallScore, 
+                    setValuesNode.VisuoSpatial, 
+                    setValuesNode.SpeedProcessing, 
+                    setValuesNode.CycleTime
+                    );
+        }
+    }
+
+}
---
+++ Assets/Calibroom/Scripts/IMLNodes/MECMSetDashboardValues.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+using InteractML;
+
+namespace MECM
+{
+    [NodeWidth(300)]
+    public class MECMSetDashboardValues : IMLNode, IFeatureIML
+    {
+        [Input] public float OverallScore;
+        [Input] public float VisuoSpatial;
+        [Input] public float SpeedProcessing;
+        [Input] public float CycleTime;
+
+        public IMLBaseDataType FeatureValues => throw new System.NotImplementedException();
+
+        public bool isExternallyUpdatable => true;
+
+        public bool isUpdated { get => m_IsUpdated; set { m_IsUpdated = value; } }
+        private bool m_IsUpdated;
+
+        public object UpdateFeature()
+        {
+            OverallScore = GetInputValue<float>("OverallScore");
+            VisuoSpatial = GetInputValue<float>("VisuoSpatial");
+            SpeedProcessing = GetInputValue<float>("SpeedProcessing");
+            CycleTime = GetInputValue<float>("CycleTime");
+            return this;
+        }
+    }
+
+}
