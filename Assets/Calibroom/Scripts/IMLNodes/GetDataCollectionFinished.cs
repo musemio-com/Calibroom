e8d0efdc25022ed8990f749c83281696a867c629
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-06-22 00:15:21 +0100

    Added node to get when the data collection is finished in the IML graph

---
+++ Assets/Calibroom/Scripts/IMLNodes/GetDataCollectionFinished.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InteractML;
+
+namespace MECM
+{
+    public class GetDataCollectionFinished : IMLNode, IUpdatableIML
+    {
+        /// <summary>
+        /// Is the data collection finished?
+        /// </summary>
+        [Output]
+        public bool DataCollectionFinished;
+
+        public bool isExternallyUpdatable => true;
+
+        public bool isUpdated { get => m_isUpdated; set => m_isUpdated = value; }
+        private bool m_isUpdated;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            UpdateEditorData._OnTaskTimeDelegate += UpdateDataCollectionFinished;
+        }
+
+        public void Update()
+        {
+            // Only allow the flag to stay true for a frame, emulating an event
+            if (DataCollectionFinished) DataCollectionFinished = false;
+        }
+
+        /// <summary>
+        /// Updates data collection finished flag to true
+        /// </summary>
+        /// <param name="emptyTime">It does nothing. Just declaring to comply with delegate</param>
+        public void UpdateDataCollectionFinished(float emptyTime)
+        {
+            DataCollectionFinished = true;
+            Debug.Log("DATA COLLECTION FINISHED IN IML GRAPH!!");
+        }
+    }
+
+}
