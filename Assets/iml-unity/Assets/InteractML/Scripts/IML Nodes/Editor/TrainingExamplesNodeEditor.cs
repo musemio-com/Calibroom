ed5e338a60e0d24eb57c47251c52e57855bd656b
Carlos Gonzalez Diaz <carlotes_247@hotmail.com>
2021-06-24 01:03:27 +0100

    Re-enabled SubFolderDataPath port in TeachTheMachine nodes

--- Assets/iml-unity/Assets/InteractML/Scripts/IML Nodes/Editor/TrainingExamplesNodeEditor.cs
+++ Assets/iml-unity/Assets/InteractML/Scripts/IML Nodes/Editor/TrainingExamplesNodeEditor.cs
@@ -191,8 +191,7 @@ namespace InteractML
             }
             
             // Added an option to specify a subfolder to save/load the data
-            // commented out for UAL students 
-            //ShowSubFolderDataField();
+            ShowSubFolderDataField();
 
             ShowTrainingExamplesDropdown();
         }
